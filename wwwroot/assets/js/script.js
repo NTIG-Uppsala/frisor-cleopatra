9dde53292c38ca3053e186a459986cc0c102c486
liljekvist <mikael0225@outlook.com>
2021-09-02 11:27:06 +0200

    Javascript to stop mobile users being able to scroll while nav menu is open

--- wwwroot/assets/js/script.js
+++ wwwroot/assets/js/script.js
@@ -1,4 +1,5 @@
 $(function(){
+    var disableScroll = false;
     $('.navTrigger').click(function () {
         $(this).toggleClass('active');
         console.log("Clicked menu");
@@ -11,21 +12,45 @@ $(function(){
         if($('.navTrigger').hasClass('active')) {
             console.log("FadeOut");
             $("#mainListDiv").fadeIn();
+            // add listener to disable scroll
+            noScroll();
         }
         else {
             console.log("FadeIn");
             $("#mainListDiv").fadeOut();
+            // Remove listener to re-enable scroll
+            enableScroll()
         }
     
     });
     
     $(window).scroll(function() {
-        if ($(document).scrollTop() > 600) {
+        if(disableScroll){
+            console.log("Doing nothing")
+        }    
+        else if (($(document).scrollTop() > 600)) {
             $('.nav').addClass('affix');
             console.log("OK");
-        } else {
+        } 
+        else {
             $('.nav').removeClass('affix');
         }
     });
+
+    function noScroll() {
+        // Get the current page scroll position
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        disableScroll = true;
+            // if any scroll is attempted, set this to the previous value
+            window.onscroll = function() {
+                window.scrollTo(scrollLeft, scrollTop);
+            };
+    }
+      
+    function enableScroll() {
+        window.onscroll = function() {};
+        disableScroll = false;
+    }
 });
 
